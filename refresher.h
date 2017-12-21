f5a0ff782df7ca1bb3c90ccc44f951249ab22654
Dan <turchiad@lafayette.edu>
2017-12-21 14:56:20 -0500

    Began.

---
+++ refresher.h
@@ -0,0 +1,12 @@
+#ifndef __REFRESHER_H_INCLUDED__
+#define __REFRESHER_H_INCLUDED__
+
+class Refresher
+{
+
+public:
+	static double bab(double num);
+	static double babHelper(double num, double guess);
+};
+
+#endif //__REFRESHER_H_INCLUDED__
